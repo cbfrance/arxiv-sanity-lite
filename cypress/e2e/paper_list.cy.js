193fe987e7f8bfb63d13590d808b44b99ec74497
Chris B. France <7366+christopherfrance@users.noreply.github.com>
2023-10-16 15:23:33 -0700

    Example generated Cypress test

--- cypress/e2e/paper_list.cy.js
+++ cypress/e2e/paper_list.cy.js
@@ -1,5 +1,15 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5000/')
+    cy.get('#paperList > .rel_paper:nth-child(1)').within(() => {
+      cy.get('.rel_score').should('exist')
+      cy.get('.rel_title').should('exist')
+      cy.get('.rel_authors').should('exist')
+      cy.get('.rel_time').should('exist')
+      cy.get('.rel_tags').should('exist')
+      cy.get('.rel_abs').should('exist')
+      cy.get('.rel_more').should('exist')
+      cy.get('.rel_inspect').should('exist')
+    })
   })
 })
